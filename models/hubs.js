fc898bcdb9716e6b70ca9afba719163eb7884ad5
Pradyumna Patil <50282477+KKKaneki@users.noreply.github.com>
2021-03-16 11:28:11 +0530

    Feat/docker/docker setup for dev prod (#9)
    * docker files setup complete prod.sh is the main file to run

--- models/hubs.js
+++ models/hubs.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
 const hubSchema = new mongoose.Schema({
-    HubID: String,
     Name: String,
     PhoneNumber: String,
-    Address: String
+    Address: String,
+    Email: String
 },{ timestamps: true });
---
+++ prod.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+docker-compose -f docker-compose.yml up -d --build
+
+# TO RUN THIS FILE
+# chmod +x prod.sh
+# ./prod.sh

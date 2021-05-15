c25005c3cbe601838ad04a4b411ba434e14f3237
Abhibhaw <asthana3110@gmail.com>
2021-05-15 05:50:56 +0530

    Separates Client Part to Bat

--- client/src/components/Logo.js
+++
@@ -1,9 +0,0 @@
-const Logo = (props) => (
-  <img
-    alt="Logo"
-    src="https://github.com/GEPTON-INFOTECH/GEPTON-INFOTECH/raw/main/branding/logo-139x52(blue-background).png"
-    {...props}
-  />
-);
-
-export default Logo;
